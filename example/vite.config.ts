bed0126fd5cf54f563569b2845cb0ec90e4ef35c
shayan-hamidi <sha.hamidi10@gmail.com>
2025-11-27 21:01:23 +0330

    fix: resolve type error

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'qrcode-pro': path.resolve(__dirname, '../dist/index.esm.js'),
+    },
+  },
 })
 
